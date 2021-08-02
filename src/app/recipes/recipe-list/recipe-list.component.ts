10bcea733b5f775179f9337ce19a96354dc3eb70
Henrietha <areathy33@gmail.com>
2021-08-02 13:41:37 +0100

    Basic set up

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Recipe} from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] =[
+    new Recipe('Jellof Rice', 'Correct Nigerian Jellof', 'https://elleyajoku.com/wp-content/uploads/2017/10/jollof-rice-recipe_.jpg'),
+    new Recipe('Jellof Rice', 'Correct Nigerian Jellof', 'https://elleyajoku.com/wp-content/uploads/2017/10/jollof-rice-recipe_.jpg')
+  ];
 
   constructor() { }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: ingredient[] = [
+    new ingredient('Rice', 34),
+    new ingredient('Tomatoes', 23)
+  ];
 
   constructor() { }
 
